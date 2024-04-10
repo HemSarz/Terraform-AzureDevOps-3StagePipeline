a897744625ce5fc665e04b4eabfed383bb9f00e5
Hemen Sarzali <124264540+HemSarz@users.noreply.github.com>
2024-04-10 20:21:25 +0200

    Update gitpushcmds.ps1

--- gitpushcmds.ps1
+++ gitpushcmds.ps1
@@ -7,13 +7,3 @@ git commit -m "UpdateTFsv1"
 
 Write-Host "Executing: git push -u origin main --force"
 git push -u origin main --force
-
-Write-Host "Running Git commands..."
-Write-Host "Executing: git add -A"
-git add -A
-
-Write-Host "Executing: git commit -m 'UpdateTFsv1'"
-git commit -m "UpdateTFsv1"
-
-Write-Host "Executing: git push -u origin main --force"
-git push -u tfaz main --force
